813a5943c992d39a7526ab977cba47c817a584b3
Dac1977 <diegodac77@gmail.com>
2022-09-21 09:47:01 -0300

    homework 1

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,14 +2,37 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+// 1010
+// 3210 posiciones > 
 
+let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum = sum + num[i] * (2 ** (num.length-1-i));
+    // numero binario 1010 
+    //        1 * 2**(3-0)
+    //        0 * 2**(3-1)
+    // ..........
+  }
+  return sum;
 }
+//console.log(BinarioADecimal('111111011'));
+
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let div = 0;
+  let resto = 0;
+  let binario =[];
+while (num > 1) {
+  resto = num % 2;
+  num = Math.floor(num / 2);
+  div = num;
+  binario.unshift(resto);
 }
-
+  binario.unshift(div);
+ return binario.join("");
+}
+//console.log(DecimalABinario(507));
 
 module.exports = {
   BinarioADecimal,
